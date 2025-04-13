be6dd4802ecf4476fc4d13384bfb8177401c793b
rahul4019 <rahulkashyap4019@gmail.com>
2025-04-13 23:09:23 +0530

    Arguments in Node

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log(
+  process.argv
+); /* prints an array containing path of the node.js executable and the path of the current file */
+
+//* argv => provides all the arguments in an array
+console.log(process.argv[2], process.argv[3]);
