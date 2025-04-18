dba2f7bee0572ffd1eb83d8c65193546ac9ad685
rahul4019 <rahulkashyap4019@gmail.com>
2025-04-18 20:58:50 +0530

    Server html file using fs.readfile method

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import { createServer } from 'node:http';
+import fs from 'node:fs/promises';
 const PORT = 3000;
 
-const server = createServer((req, res) => {
-//   console.log('req: ', req);
+const server = createServer(async (req, res) => {
+  //   console.log('req: ', req);
   const url = req.url;
 
   if (url === '/') {
     res.writeHead(200, { 'content-type': 'text/html' });
 
-    res.end('<h1>Hello from Node.js server!</h1>');
+    const data = await fs.readFile('./index.html');
+
+    // res.end('<h1>Hello from Node.js server!</h1>');
+    res.end(data);
   } else if (url === '/about') {
     res.writeHead(200, { 'content-type': 'text/html' });
 
