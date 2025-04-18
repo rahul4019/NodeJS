f509462785c7d36795b2d1991ff96679dbc88830
rahul4019 <rahulkashyap4019@gmail.com>
2025-04-18 22:21:40 +0530

    Use streams to serve html file

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 import { createServer } from 'node:http';
-import fs from 'node:fs/promises';
+import fs from 'node:fs';
 const PORT = 3000;
 
 const server = createServer(async (req, res) => {
+  //* req is a readable stream
   //   console.log('req: ', req);
   const url = req.url;
 
   if (url === '/') {
     res.writeHead(200, { 'content-type': 'text/html' });
 
-    const data = await fs.readFile('./index.html');
+    // const data = await fs.readFile('./index.html');
+
+    const dataStream = fs.createReadStream('./index.html');
+    
+    //* data event on stream
+    dataStream.on('data',(chunk) => {
+        //* res is a writeable stream
+        res.write(chunk)
+    })
+
+    //* end event on stream
+    dataStream.on('end',() => {
+        res.end()
+    })
 
     // res.end('<h1>Hello from Node.js server!</h1>');
-    res.end(data);
+    // res.end(data);
   } else if (url === '/about') {
     res.writeHead(200, { 'content-type': 'text/html' });
 
